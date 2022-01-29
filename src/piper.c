6a15646d82127af967b1a7de516bfe4cd66cdebe
luke <hoskam@pdx.edu>
2022-01-29 12:51:40 -0800

    program counts number of elements

--- src/piper.c
+++ src/piper.c
@@ -3,12 +3,29 @@
 #include <string.h>
 #include <unistd.h>
 
-
+#define BUFFSIZE 10
 
 int main(int argc, char *argv[])
 {
+    int n;
+    char buf[BUFFSIZE];
+    char **stringArray;
+    int elementCount = 0;
 
-
-
+    while ((n = read(STDIN_FILENO, buf, BUFFSIZE)) > 0) // Read from STDIN and set the number read to n
+    {
+        if(n <0)
+        {
+            perror("read error");
+        }
+        for (int i = 0; i<n; i++)
+        {
+            if(buf[i] == ' ' || buf[i] == '\n')
+            {
+                elementCount++;
+            }
+        }
+    }
+    printf("%d elements\n", elementCount);
     return 0;
 }
