6aacb57ddd4fe99efc72477e964c5d52ce57b84f
luke <hoskam@pdx.edu>
2022-01-30 06:57:05 -0800

    chreates one string from stdin

--- src/piper.c
+++ src/piper.c
@@ -2,30 +2,39 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <ctype.h>
 
-#define BUFFSIZE 10
+#define BUFFSIZE 5000
 
 int main(int argc, char *argv[])
 {
     int n;
     char buf[BUFFSIZE];
-    char **stringArray;
+    char stringArray[BUFFSIZE];
     int elementCount = 0;
 
     while ((n = read(STDIN_FILENO, buf, BUFFSIZE)) > 0) // Read from STDIN and set the number read to n
     {
-        if(n <0)
+        if (n < 0)
         {
             perror("read error");
         }
-        for (int i = 0; i<n; i++)
+        for (int i = 0; i < n; i++)
         {
-            if(buf[i] == ' ' || buf[i] == '\n')
+            if (isspace(buf[i]))
             {
-                elementCount++;
+                if (stringArray[i - 1] != ' ')
+                {
+                    stringArray[elementCount++] = ' ';
+                }
+            }
+            else
+            {
+                stringArray[elementCount++] = buf[i];
             }
         }
     }
     printf("%d elements\n", elementCount);
+    printf("string array: %s\n", stringArray);
     return 0;
 }
