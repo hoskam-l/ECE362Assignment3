fa8c9633e3947cad1f5c14729e45df9cd05cc7e9
ht145 <92292027+ht145@users.noreply.github.com>
2022-01-31 16:43:08 -0800

    files contain itoa removed and error checking added to piper.c

--- src/timer.c
+++ src/timer.c
@@ -26,43 +26,6 @@ void printLine(const char *line)
         if (write(STDOUT_FILENO, "\n", 2) != 2)
                 perror("write error");
 }
-// from: https://stackoverflow.com/questions/8257714/how-to-convert-an-int-to-string-in-c
-/**
- * C++ version 0.4 char* style "itoa":
- * Written by Lukás Chmela
- * Released under GPLv3.
- */
-char *itoa(int value, char *result, int base)
-{
-        // check that the base if valid
-        if (base < 2 || base > 36)
-        {
-                *result = '\0';
-                return result;
-        }
-
-        char *ptr = result, *ptr1 = result, tmp_char;
-        int tmp_value;
-
-        do
-        {
-                tmp_value = value;
-                value /= base;
-                *ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz"[35 + (tmp_value - value * base)];
-        } while (value);
-
-        // Apply negative sign
-        if (tmp_value < 0)
-                *ptr++ = '-';
-        *ptr-- = '\0';
-        while (ptr1 < ptr)
-        {
-                tmp_char = *ptr;
-                *ptr-- = *ptr1;
-                *ptr1++ = tmp_char;
-        }
-        return result;
-}
 
 // exec_prog from: https://stackoverflow.com/questions/5237482/how-do-i-execute-external-program-within-c-code-in-linux-with-arguments
 // This hardly resembles the orginal program.
@@ -145,7 +108,8 @@ int main(int argc, char *argv[])
         // Create a string to hold the end time value
         char strTime[sizeof(int) + 1];
         // convert the int value of the total time to a string
-        itoa((int)end, strTime, 10);
+         snprintf(strTime, sizeof(int) +1 , "%d", end);  
+
         //create a temp string (tgt) the proper size of strTime and beginString
         size_t len1 = strlen(beginString), len2 = strlen(strTime);
         char *tgt = (char *)malloc(len1 + len2 + 1);
